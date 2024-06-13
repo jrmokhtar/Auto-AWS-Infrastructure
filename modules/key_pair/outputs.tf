63c52ab5cf2588555f1b6843563b55d97734a5d0
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-06-13 19:15:50 +0300

    Add files via upload

---
+++ modules/key_pair/outputs.tf
@@ -0,0 +1,13 @@
+output "private_key_pem" {
+  value     = tls_private_key.nader.private_key_pem
+  sensitive = true
+}
+
+
+output "key_name" {
+  value = aws_key_pair.nader.key_name
+}
+
+output "private_key_path" {
+  value = local_file.private_key.filename
+}
---
+++ modules/key_pair/variables.tf
@@ -0,0 +1,5 @@
+variable "key_name" {
+  description = "The name of the key pair"
+  type        = string
+   default = "nader"
+}
