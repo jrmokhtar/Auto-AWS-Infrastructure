29e61202812e53d58f46298808f4c16a336a33cc
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-06-19 23:03:13 +0300

    main.tf

--- modules/key_pair/main.tf
+++ modules/key_pair/main.tf
@@ -1,10 +1,10 @@
-resource "tls_private_key" "nader" {
+resource "tls_private_key" "name" {
   algorithm   = "RSA"
   rsa_bits    = 2048
 }
 
 resource "local_file" "private_key" {
-  content  = tls_private_key.nader.private_key_pem
+  content  = tls_private_key.name.private_key_pem
   filename = "ec2_key_pair.pem"
   provisioner "local-exec" {
     command = "chmod 400 ec2_key_pair.pem"
@@ -12,7 +12,7 @@ resource "local_file" "private_key" {
 }
 
 
-resource "aws_key_pair" "nader" {
+resource "aws_key_pair" "name" {
   key_name   = var.key_name
-  public_key = tls_private_key.nader.public_key_openssh
+  public_key = tls_private_key.name.public_key_openssh
 }
